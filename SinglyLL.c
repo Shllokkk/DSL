6542f3f8e462f19d8adeeee469c952f22b381407
Shllokkk <shllokosan23@gmail.com>
2023-09-05 18:38:47 +0530

    adding on 05/09/23

---
+++ SinglyLL.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+void createNode();
+void insertBeg();
+void display();
+
+void main()
+{
+    
+}
