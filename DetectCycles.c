31e54153adca4345ae5db9de993908d3c312dd8e
Shllokkk <shllokosan23@gmail.com>
2024-02-09 17:03:23 +0530

    adding on 09/02/24

--- DetectCycles.c
+++ DetectCycles.c
@@ -1,40 +1,65 @@
 #include<stdio.h>
 #define MAXVERTICES 100
 
-int graph[MAXVERTICES][MAXVERTICES];
+// int graph[MAXVERTICES][MAXVERTICES];
+int graph[4][4] = { {0,1,0,1},
+                    {1,0,1,0},
+                    {0,1,0,1},
+                    {1,0,1,0}};
 int visited[MAXVERTICES];
-int numnodes;
+int numnodes=4;
 
-void dfs(int startVertex)
+int dfs(int initialVertex, int parent)
 {
-    visited[startVertex]=1;
-    printf("\n%d",startVertex);
+    visited[initialVertex]=1;
 
     for(int i=0;i<numnodes;i++)
     {
-        if(graph[startVertex][i] && !visited[i])
-            dfs(i);
+        if(graph[initialVertex][i])
+        {
+            if(!visited[i])
+            {
+                if(dfs(i,initialVertex))
+                    return 1;
+            }
+            else if(i!=parent)
+                return 1;   
+        }
     }
+    return 0;
+}
+
+int detectCycle()
+{
+    for(int i=0;i<numnodes;i++)
+    {
+        if(!visited[i])
+            if(dfs(i,-1))
+                return 1;
+    }
+    return 0;
 }
 
 void main()
 {
     int startVertex;
 
-    printf("\nEnter number of nodes for an undirected graph: ");
-    scanf("%d",&numnodes);
+    // printf("\nEnter number of nodes for an undirected graph: ");
+    // scanf("%d",&numnodes);
 
-    printf("\nEnter adjacency matrix ");
+    //printf("\nEnter adjacency matrix ");
 
-    for(int i=0;i<numnodes;i++)
-    {
-            for(int j=0;j<numnodes;j++)
-            {
-                printf("Graph[%d][%d]: ",i,j);
-                scanf("%d",&graph[i][j]);
-            }
-    }
-    printf("\nEnter the start vertex: ");
-    scanf("%d",&startVertex);
-    dfs(startVertex);
+    // for(int i=0;i<numnodes;i++)
+    // {
+    //         for(int j=0;j<numnodes;j++)
+    //         {
+    //             printf("Graph[%d][%d]: ",i,j);
+    //             scanf("%d",&graph[i][j]);
+    //         }
+    // }
+
+    if(detectCycle())
+        printf("\nCycle detected in graph");
+    else    
+        printf("\nNo cycle detected in graph");
 }
